69ded465f3a40c101abc8973e8130be313543fe5
Josip Kelecic <kelecicj@gmail.com>
2024-11-09 11:57:47 -0300

    sm8550-common: Add shim for libhypervintf * Needed by camera, we don't use hyper HAL.
    Change-Id: I7374d546c7c50ad7910c28ecd986096f8d99efc6

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -43,8 +43,7 @@ function lib_to_package_fixup_vendor_variants() {
     case "$1" in
         vendor.qti.diaghal@1.0 | \
             libsecril-client | \
-            vendor.qti.hardware.fm@1.0 | \
-            libhyper)
+            vendor.qti.hardware.fm@1.0)
             echo "$1_vendor"
             ;;
         libagmclient | \
