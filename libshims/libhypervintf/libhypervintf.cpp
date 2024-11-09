a60846e2126b51b88f78932cdd28547e605a1481
chaptsand <chaptsand@gmail.com>
2024-11-09 12:12:00 -0300

    sm8550-common: libshims: Add missing symbol * Needed by camera after removing hyper HAL
    Co-authored-by: pckotzer <thegamethis@outlook.com>
    Change-Id: I7becb51fcbbd913f3dd2821b6109bbc97dc5501c

--- libshims/libhypervintf/libhypervintf.cpp
+++ libshims/libhypervintf/libhypervintf.cpp
@@ -5,6 +5,10 @@
  */
 
 #include <string>
+#include <vector>
+
+struct HyPerToken
+{};
 
 void hyper_init()
 {}
@@ -14,3 +18,29 @@ void hyper_checkSysfsIdExist(int)
 
 void hyper_writeSysfs(int, std::string)
 {}
+
+void hyper_acquire(HyPerToken, std::vector<int>)
+{}
+
+HyPerToken hyper_getNewHyPerToken() {
+    return HyPerToken();
+}
+
+extern "C" void _Z22hyper_setTokenProcNameP10HyPerTokenNSt3__112basic_stringIcNS1_11char_traitsIcEENS1_9allocatorIcEEEE(HyPerToken token, std::string procName) {
+    (void)token;
+    (void)procName;
+}
+
+bool hyper_checkResourceExist(int) {
+    return false;
+}
+
+void hyper_release(HyPerToken)
+{}
+
+void hyper_setProcName(std::string)
+{}
+
+int hyper_getSupportedFrequency(int, int) {
+    return 0;
+}
